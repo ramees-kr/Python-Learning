e62969bdb19170b3d4547e4465f3d50768cab4d2
ramees-kr <ramees.kr@outlook.com>
2021-10-17 11:20:09 -0700

    String and escapes

---
+++ Strings.py
@@ -0,0 +1,11 @@
+print('spam eggs')  # single quotes
+
+print('doesn\'t')  # use \' to escape the single quote...
+
+print("doesn't")  # ...or use double quotes instead
+
+print('"Yes," they said.') # double or single quotes inside any one of the other are automaticaly escaped
+
+print("\"Yes,\" they said.") # escape character
+
+print('"Isn\'t," they said.') # double quotes are automatically escaped, single quotes 
