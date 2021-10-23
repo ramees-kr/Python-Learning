bedfa4a0ba1792e39de76efb4fc10b54f30eb596
ramees-kr <ramees.kr@outlook.com>
2021-10-23 11:32:33 -0700

    More strings

--- Strings.py
+++ Strings.py
@@ -1,3 +1,6 @@
+from typing import Text
+
+
 print('spam eggs')  # single quotes
 
 print('doesn\'t')  # use \' to escape the single quote...
@@ -13,3 +16,11 @@ print('"Isn\'t," they said.') # double quotes are automatically escaped, single 
 print('''I dont know 
             what this 
 does''')                   #If we use triple quotes, it will print the way it is given in multiple lines
+
+Word = 'Python'
+print(Word[0])          #you can index string from left to right using index 0
+print(Word[-0])         #you can index string from right to left using negative numbers
+print(Word[-1])         #since -0 is the same as 0, negative indices start from -1.
+
+print(type(Word[-1]))   #unlike C or C++, there is only one class 'string' #no char data type
+print(type(Word))
